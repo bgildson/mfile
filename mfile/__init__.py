4c6f26b3c7e9a1df6c9169fb4aedb68d0bd94c6c
gildson <Gildson13@hotmail.com>
2015-06-19 17:56:21 -0300

    Adicionando atributos date_creation|date_modification|size|remove e funcionalidade de setup e adicionando o arquivo .gitignore

--- mfile/__init__.py
+++ mfile/__init__.py
@@ -2,12 +2,21 @@
 
 class MFile:
 
+    def _exists(func):
+        def params(self, *args, **kwargs):
+            if not self.exists:
+                raise Exception('File not exists!')
+            return func(self, *args, **kwargs)
+        return params
+
     def __init__(self, path):
+        from datetime import datetime
         import shutil
-        if not shutil.os.path.exists(path) or not shutil.os.path.isfile(path):
-            raise Exception('File not exist!')
-        self._path = path
+        self._d = datetime
         self._s = shutil
+        if not shutil.os.path.exists(path) or not shutil.os.path.isfile(path):
+            raise Exception('File not exists!')
+        self._path = path
 
     def __repr__(self):
         return "<File: '%s'>" % self.path
@@ -16,10 +25,21 @@ class MFile:
     def directory(self):
         return self._s.os.path.dirname(self._path)
 
+    @property
+    @_exists
+    def date_creation(self):
+        return self._d.fromtimestamp(self._s.os.path.getctime(self.path))
+
+    @property
+    @_exists
+    def date_modification(self):
+        return self._d.fromtimestamp(self._s.os.path.getmtime(self.path))
+
     @directory.setter
+    @_exists
     def directory(self, value):
         if not self._s.os.path.exists(value):
-            raise Exception('Destination directory does not exist!')
+            raise Exception('Destination directory does not exists!')
         self.path = self._s.os.path.join(value, self.full_name)
 
     @property
@@ -31,6 +51,7 @@ class MFile:
         return self.path.split('.')[-1]
 
     @extension.setter
+    @_exists
     def extension(self, value):
         new_path = self._s.os.path.join(self.directory, self.name + '.' + value)
         self._s.os.rename(self._path, new_path)
@@ -45,6 +66,7 @@ class MFile:
         return '.'.join(self._path.split(self._s.os.path.sep)[-1].split('.')[:-1])
 
     @name.setter
+    @_exists
     def name(self, value):
         new_path = self._s.os.path.join(self.directory, value + '.' + self.extension)
         self._s.os.rename(self._path, new_path)
@@ -55,12 +77,18 @@ class MFile:
         return self._path
 
     @path.setter
+    @_exists
     def path(self, value):
         if not self._s.os.path.exists(self._s.os.path.dirname(value)):
             raise Exception('Destination directory does not exist!')
         self._s.move(self._path, value)
         self._path = value
 
+    @property
+    def size(self):
+        return self._s.os.path.getsize(self.path)
+
+    @_exists
     def copy(self, destine, select_copied=False):
         self._s.copy(self._path, destine)
         if select_copied: self._path = self._s.os.path.join(destine, self.full_name)
@@ -68,8 +96,13 @@ class MFile:
     def move(self, destine):
         self.directory = destine
 
+    @_exists
     def open(self):
         self._s.os.startfile(self._path)
+
+    @_exists
+    def remove(self):
+        self._s.os.remove(self._path)
 
     def rename(self, name):
         self.name = name
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='MFile',
+    version='0.1',
+    url='https://github.com/gildson/mfile',
+    license='BSD',
+    author='Gildson Bezerra',
+    author_email='gildson13@hotmail.com',
+    description='Managing file with Python Object',
+    long_description='',
+    packages=['mfile'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Libraries :: Python Modules'
+        ]
+    )
